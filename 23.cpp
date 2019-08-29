f0cbd2b03e9a88f85ac474ca34ecb5a38c22b4e0
Bon <550211713@qq.com>
2019-08-29 14:09:40 +0800

    23

--- 23.cpp
+++ 23.cpp
@@ -1,4 +1,5 @@
 ﻿#include "pch.h"
+#include "heap.h"
 
 /*  ##description
 	合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
@@ -13,19 +14,74 @@
 ]
 输出: 1->1->2->3->4->4->5->6
 
+普通扫描：O(n*合并后总长度)
+heap： O(logn*合并后总长度)
+
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/merge-k-sorted-lists
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
-class Solution {
+class Solution23 {
 public:
 	ListNode* mergeKLists(vector<ListNode*>& lists) {
-		
+		ListNode* head = new ListNode(-1);
+		ListNode* tail = head;
+
+		Heap<ListNode*> heap([](const ListNode* a, const ListNode* b) {return a->val < b->val; });
+		try
+		{
+			for (const auto& list : lists)
+				if (list)
+					heap.insert(list);
+
+			while (true)
+			{
+				ListNode* tmp = heap.pop();
+				if (tmp == nullptr)
+					continue;
+				tail->next = tmp;
+				tail = tmp;
+				if (tmp->next != nullptr)
+					heap.insert(tmp->next);
+			}
+		}
+		catch (std::invalid_argument& e)
+		{
+		}
+
+		tail = head->next;
+		delete head;
+		return tail;
 	}
 };
 
-TEST(leetcode, testing_)
+TEST(leetcode, 23)
 {
-	Solution s;
+	vector<ListNode*> lists;
+	ListNode* head = new ListNode(1);
+	head->next = new ListNode(4);
+	head->next->next = new ListNode(5);
+	head->next->next->next = new ListNode(8);
+	lists.push_back(head);
+
+	head = new ListNode(1);
+	head->next = new ListNode(3);
+	head->next->next = new ListNode(4);
+	lists.push_back(head);
+
+	head = nullptr;
+	lists.push_back(head);
+
+	head = new ListNode(2);
+	head->next = new ListNode(6);
+	head->next->next = new ListNode(7);
+	lists.push_back(head);
+
+	head = nullptr;
+	lists.push_back(head);
+
+	Solution23 s;
+	head = s.mergeKLists(lists);
+	showAndDeleteList(head);
 }
---
+++ heap.h
@@ -0,0 +1,92 @@
+#pragma once
+#include <functional>
+
+template<typename T>
+class Heap
+{
+public:
+	Heap() = default;
+	Heap(std::function<bool(const T&, const T&)> cmp) :comparator_(cmp) {}
+	Heap(std::vector<T>& data, std::function<bool(const T&, const T&)> cmp = NULL) :data_(data)
+	{
+		if (cmp != NULL)
+			comparator_ = cmp;
+		build();
+	}
+	~Heap() = default;
+
+	void insert(const T& value)
+	{
+		data_.push_back(value);
+		heap_up(data_.size() - 1);
+	}
+
+	T pop()
+	{
+		if (data_.empty())
+			throw std::invalid_argument("Heap empty");
+		T rst = std::move(data_[0]);
+		data_[0] = std::move(data_[data_.size() - 1]);
+		data_.pop_back();
+		heap_down(0);
+		return rst;
+	}
+
+	T pop_and_insert(const T& value)
+	{
+		if (data_.empty())
+			throw std::invalid_argument("Heap empty");
+		T rst = std::move(data_[0]);
+		data_[0] = value;
+		heap_down(0);
+		return rst;
+	}
+
+private:
+
+	std::function<bool(const T&, const T&)> comparator_ = [](const T& a, const T& b) { return a < b; };
+
+	static int father(size_t index) { return (index - 1) / 2; }
+
+	void swap(size_t a, size_t b)
+	{
+		T tmp = std::move(data_[a]);
+		data_[a] = std::move(data_[b]);
+		data_[b] = std::move(tmp);
+	}
+
+	void build()
+	{
+		for (int index = data_.size() / 2 - 1; index >= 0; index--)
+			heap_down(index);
+	}
+
+	void heap_up(size_t index)
+	{
+		if (index <= 0) 
+			return;
+		if (!comparator_(data_[father(index)], data_[index]))
+		{
+			swap(father(index), index);
+			heap_up(father(index));
+		}
+	}
+
+	void heap_down(size_t index)
+	{
+		if (index >= data_.size() / 2)
+			return;
+		size_t less_index;
+		if (index * 2 + 2 >= data_.size())
+			less_index = index * 2 + 1;
+		else 
+			less_index = comparator_(data_[index * 2 + 1], data_[index * 2 + 2]) ? index * 2 + 1 : index * 2 + 2;
+		if (!comparator_(data_[index], data_[less_index]))
+		{
+			swap(index, less_index);
+			heap_down(less_index);
+		}
+	}
+
+	std::vector<T> data_;
+};
